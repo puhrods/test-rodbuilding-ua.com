bc35ecae179c2d5498302a997802f92f840b33b3
andrey.intent <andrey.intent@gmail.com>
2024-01-18 10:46:50 +0200

    fix package

--- system/library/cart/cart.php
+++ system/library/cart/cart.php
@@ -33,7 +33,7 @@ class Cart {
 	public function getProducts() {
 		$product_data = array();
 
-		$cart_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "cart WHERE api_id = '" . (isset($this->session->data['api_id']) ? (int)$this->session->data['api_id'] : 0) . "' AND customer_id = '" . (int)$this->customer->getId() . "' AND session_id = '" . $this->db->escape($this->session->getId()) . "'");
+		$cart_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "cart WHERE api_id = '" . (isset($this->session->data['api_id']) ? (int)$this->session->data['api_id'] : 0) . "' AND customer_id = '" . (int)$this->customer->getId() . "' AND session_id = '" . $this->db->escape($this->session->getId()) . "' ORDER BY cart_id ASC");
 
 		foreach ($cart_query->rows as $cart) {
 			$stock = true;
